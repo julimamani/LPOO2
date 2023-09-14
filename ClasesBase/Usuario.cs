809860f41bff1eb3e48864ac159b4f72683bb3ac
Agustina Cinthia Mamani <agustina.khalifaa@gmail.com>
2023-09-14 00:37:53 -0300

    se agrego proyecto vista y login de usuario

--- ClasesBase/Usuario.cs
+++ ClasesBase/Usuario.cs
@@ -7,59 +7,22 @@ namespace ClasesBase
 {
     public class Usuario
     {
-        public string UserName
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
+        public string UserName;
+        public string Password;
+        public string Apellido;
+        public string Nombre;
+        public string Rol;
+
+        public Usuario(string username, string pass, string apellido, string nombre, string rol) {
+            UserName = username;
+            Password = pass;
+            Apellido = apellido;
+            Nombre = nombre;
+            Rol = rol;
         }
 
-        public string Password
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
+        
 
-        public string Apellido
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
-
-        public string Nombre
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
-
-        public string Rol
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
+       
     }
 }
---
+++ Vistas/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Usuario admin;
+        private Usuario operador;
+        public MainWindow()
+        {
+            InitializeComponent();
+            inicializarUsuarios();
+        }
+
+        public void inicializarUsuarios(){
+            admin = new Usuario( "admin", "admin","perez", "pepe", "1");
+            operador = new Usuario("operador", "operador", "cordova", "luis", "2");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if ((textUser.Text == admin.UserName && passBox.Password == admin.Password) || (textUser.Text == operador.UserName && passBox.Password == operador.Password))
+            {
+                MessageBox.Show("Usuario o contraseña correcta", "INFO", MessageBoxButton.OK);
+            }
+            else {
+                MessageBox.Show("Usuario o contraseña incorrecta", "ERROR", MessageBoxButton.OK);
+            }
+        }
+    }
+}
