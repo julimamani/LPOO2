2817471961251f55bf1e39498c058449f8298f7b
CristianSBorja <lobo.23ag.18@gmail.com>
2023-09-26 21:11:48 -0300

    hice el punto 5 de llamar a la ventana y que muestre los roles de admi y operador

---
+++ ClasesBase/Rol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    class Rol
+    {
+        private int rol_codigo;
+        private string rol_descripcion;
+
+        public Rol(int codigo,string descripcion){
+            rol_codigo = codigo;
+            rol_descripcion = descripcion;
+        }
+
+        public Rol() { 
+        
+        }
+
+        public int codigo
+        {
+            get { return rol_codigo; }
+            set { rol_codigo = value; }
+        }
+        public string descripcion
+        {
+            get { return rol_descripcion; }
+            set { rol_descripcion = value; }
+        }
+       
+    }
+}
