146ea2a4e1dac7ba8782b8fda31ed3dde80884f3
Cinthia Agustina Mamani <agustina.khalifaa@gmail.com>
2023-10-23 23:49:20 -0300

    se agrego grilla tipos de vehiculo y consulta a la base de datos

---
+++ ClasesBase/TrabajarTiposVehiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarTiposVehiculo
+    {
+        public DataTable TraerTiposVehiculo()
+        {
+            using (DataTable dt = new DataTable())
+            {
+                using (SqlConnection cn = new SqlConnection(ClasesBase.Properties.Settings.Default.playaConnectionString))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter("Select * From TipoVehiculo", cn))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+    }
+}
+
