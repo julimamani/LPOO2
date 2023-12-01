fd07ce95aac11311074597dc22b68f41adff2464
julimamani <65739081+julimamani@users.noreply.github.com>
2023-11-30 22:39:53 -0300

    Update ABMcliente.xaml.cs
    punto 6

--- Vistas/ABMcliente.xaml.cs
+++ Vistas/ABMcliente.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,186 +12,195 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ClasesBase;
+using System.Data.SqlClient;
+using Microsoft.Win32;
+
 
 namespace Vistas
 {
-    public partial class ABMcliente : Page
+    /// <summary>
+    /// Interaction logic for ABMtipoVehiculo.xaml
+    /// </summary>
+    public partial class ABMtipoVehiculo : Page
     {
-        Cliente cliente = new Cliente();
-
-        public ABMcliente()
+        public ABMtipoVehiculo()
         {
             InitializeComponent();
-            this.DataContext = new Cliente();
+        }
+
+        private Boolean form_Completo()
+        {
+            MessageBoxResult Result;
+
+            Result = MessageBox.Show("¿Quiere guardar los datos?", "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (Result == MessageBoxResult.Yes)
+                return true;
+            else
+                return false;
+        }
+
+
+        private bool controlDatos()
+        {
+            if (txtCodigo.Text == "" || txtDescripcion.Text == "" || txtTarifa.Text == "")
+                return false;
+            else
+                return true;
+        }
+
+        private void limpiarForm()
+        {
+            txtCodigo.Text = txtCodigo.Text;
+            txtDescripcion.Text = string.Empty;
+            txtTarifa.Text = string.Empty;
+
+        }
+
+
+        private void GuardarTipoVehiculo_Click(object sender, RoutedEventArgs e)
+        {
+            if (form_Completo())
+            {
+                if (controlDatos())
+                {
+                    TipoVehiculo tipoVehiculo = new TipoVehiculo();
+                    tipoVehiculo.TVCodigo = txtCodigo.Text;
+                    tipoVehiculo.Descripcion = txtDescripcion.Text;
+                    tipoVehiculo.Tarifa = txtTarifa.Text;
+                    tipoVehiculo.Imagen = Imagen_Url.Text; // Guarda la ruta de la imagen
+
+                    try
+                    {
+                        // Intenta insertar el tipo de vehículo
+                        TrabajarTiposVehiculo.insertarTipoVehiculo(tipoVehiculo);
+                        limpiarForm();
+                        MessageBox.Show("Tipo de vehículo guardado con éxito.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Maneja la excepción lanzada por el método insertarTipoVehiculo
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("TODOS LOS CAMPOS DEBEN ESTAR CARGADOS Y LA TARIFA DEBE SER UN NÚMERO VÁLIDO", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("TODOS LOS CAMPOS DEBEN ESTAR CARGADOS", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void CargarImagen_Click(object sender, RoutedEventArgs e)
+{
+    OpenFileDialog openFileDialog = new OpenFileDialog();
+    openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.gif|Todos los archivos|*.*";
+
+    if (openFileDialog.ShowDialog() == true)
+    {
+        string rutaImagen = openFileDialog.FileName;
+        Imagen_Url.Text = rutaImagen;
+
+        // Muestra la imagen en el control Image
+        BitmapImage bitmap = new BitmapImage(new Uri(rutaImagen));
+        imgVehiculo.Source = bitmap;
+    }
+}
+
+        private void ModificarTipoVehiculo_Click(object sender, RoutedEventArgs e)
+        {
+            /*string codigoTipoVehiculo = txtCodigo.Text;
+
+            if (string.IsNullOrEmpty(codigoTipoVehiculo))
+            {
+                MessageBox.Show("Ingresa un código de tipo de vehículo válido.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }*/
+
+            if (form_Completo() && controlDatos())
+            {
+                try
+                {
+                    TrabajarTiposVehiculo trabajarTiposVehiculo = new TrabajarTiposVehiculo();
+                    
+                        // The type of vehicle exists, so proceed with the modification
+                        TipoVehiculo tipoVehiculo = new TipoVehiculo();
+                        tipoVehiculo.TVCodigo = txtCodigo.Text;
+                        tipoVehiculo.Descripcion = txtDescripcion.Text;
+                        tipoVehiculo.Tarifa = txtTarifa.Text;
+
+                        TrabajarTiposVehiculo.ActualizarTipoVehiculo(tipoVehiculo);
+                        MessageBox.Show("Tipo de vehículo modificado con éxito.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                        limpiarForm();
+                        Content = new TiposVehiculos();
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al modificar el tipo de vehículo: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Verifica que todos los campos estén completos y que la tarifa sea un número válido.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void EliminarTipoVehiculo_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtén el código del tipo de vehículo desde txtCodigo.Text
+            string codigoTipoVehiculo = txtCodigo.Text;
+
+            // Verifica si el campo está vacío
+            if (string.IsNullOrEmpty(codigoTipoVehiculo))
+            {
+                MessageBox.Show("El campo del código está vacío. Ingresa un código válido.", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                // Verifica si el código existe en la base de datos antes de intentar eliminar
+                TrabajarTiposVehiculo trabajarTiposVehiculo = new TrabajarTiposVehiculo();
+                if (trabajarTiposVehiculo.TipoVehiculoExisteEnBaseDeDatos(codigoTipoVehiculo))
+                {
+                    // Llama al método EliminarTipoVehiculo para eliminar el tipo de vehículo
+                    TrabajarTiposVehiculo.EliminarTipoVehiculo(codigoTipoVehiculo);
+                    MessageBox.Show("Tipo de vehículo eliminado con éxito.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                    // Limpia los campos (reemplaza esto con tu función para limpiar los campos)
+                    limpiarForm();
+                }
+                else
+                {
+                    // Muestra un mensaje de error si el código no existe en la base de datos
+                    MessageBox.Show("El código de tipo de vehículo no existe en la base de datos.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Muestra un mensaje de error en caso de excepción
+                MessageBox.Show("Error al eliminar el tipo de vehículo: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void LimpiarForm_Click(object sender, RoutedEventArgs e)
         {
-            LimpiarForm();
+            limpiarForm();
         }
-        
 
-        private void LimpiarForm()
+        private void btnVerTiposVehiculos_Click(object sender, RoutedEventArgs e)
         {
-            txtDNI.Text = string.Empty;
-            txtApellido.Text = string.Empty;
-            txtNombre.Text = string.Empty;
-            txtTelefono.Text = string.Empty;
+            //TiposVehiculos listadoTiposVehiculos = new TiposVehiculos();
+            //listadoTiposVehiculos.Show();
         }
 
-        private void GuardarCliente_Click(object sender, RoutedEventArgs e)
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
         {
-            // Cargar los datos del cliente desde los campos de texto u otros controles
-            cliente.ClienteDNI = int.Parse(txtDNI.Text);
-            cliente.Nombre = txtNombre.Text;
-            cliente.Apellido = txtApellido.Text;
-            cliente.Telefono = txtTelefono.Text;
-            // Asigna otros campos si es necesario
-
-            // Luego, verifica si hay errores en la instancia del cliente
-            string nombreError = cliente["Nombre"];
-            string apellidoError = cliente["Apellido"];
-            string telefonoError = cliente["Telefono"];
-
-            if (string.IsNullOrEmpty(nombreError) && string.IsNullOrEmpty(apellidoError) && string.IsNullOrEmpty(telefonoError))
-            {
-                // Los datos son válidos, continúa con el proceso de guardado
-                try
-                {
-                    TrabajarClientes.InsertarCliente(cliente);
-                    MessageBox.Show("Cliente guardado con éxito.");
-                    LimpiarForm();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error al crear el cliente: " + ex.Message);
-                }
-            }
-            else
-            {
-                // Muestra mensajes de error o realiza acciones según corresponda
-                MessageBox.Show("Por favor, complete todos los campos correctamente antes de guardar.");
-            }
+            Content = new TiposVehiculos();
         }
-        private void EliminarCliente_Click(object sender, RoutedEventArgs e)
-        {
-            // Solicitar confirmación al usuario antes de eliminar el cliente
-            MessageBoxResult result = MessageBox.Show("¿Está seguro que desea eliminar el Cliente seleccionado?", "Eliminar Cliente", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
-            if (result == MessageBoxResult.No)
-                return;
-
-            // Obtener el DNI ingresado por el usuario
-            string dni = txtDNI.Text;
-
-            if (!string.IsNullOrEmpty(dni))
-            {
-                try
-                {
-                    // Verifica el valor de txtDNI.Text antes de llamar al método EliminarCliente.
-                    int dniCliente = int.Parse(dni);
-
-                    // Llama al método de ClasesBase para eliminar el cliente de la base de datos
-                    TrabajarClientes.EliminarCliente(dniCliente);
-
-                    // Muestra un mensaje de éxito
-                    MessageBox.Show("Cliente eliminado con éxito.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
-
-                    // Limpia los campos (reemplaza esto con tu función para limpiar los campos)
-                    LimpiarForm();
-                }
-                catch (Exception ex)
-                {
-                    // Muestra un mensaje de error en caso de excepción
-                    MessageBox.Show("Error al eliminar el cliente: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Por favor, ingrese un DNI válido para eliminar el cliente.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-        }
-
-
-
-        private void ModificarCliente_Click(object sender, RoutedEventArgs e)
-        {
-            if (form_Completo() && controlDatos())
-            {
-                CargarDatos();
-
-                try
-                {
-                    // Llama al método de ClasesBase para actualizar el cliente en la base de datos
-                    TrabajarClientes.ActualizarCliente(cliente);
-                    MessageBox.Show("Cliente modificado con éxito.");
-                    LimpiarForm();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error al modificar el cliente: " + ex.Message);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Por favor, complete todos los campos antes de modificar.");
-            }
-        }
-
-        private bool form_Completo()
-        {
-            MessageBoxResult Result;
-
-            Result = MessageBox.Show("¿Quiere guardar los datos?", "Confirmación ", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            return Result == MessageBoxResult.Yes;
-        }
-
-        private void CargarDatos()
-        {
-            cliente.ClienteDNI = int.Parse(txtDNI.Text);
-            cliente.Apellido = txtApellido.Text;
-            cliente.Nombre = txtNombre.Text;
-            cliente.Telefono = txtTelefono.Text;
-        }
-
-        private bool controlDatos()
-        {
-            return !string.IsNullOrEmpty(txtDNI.Text) &&
-                   !string.IsNullOrEmpty(txtApellido.Text) &&
-                   !string.IsNullOrEmpty(txtNombre.Text) &&
-                   !string.IsNullOrEmpty(txtTelefono.Text);
-        }
-
-        private void txtDNI_LostFocus(object sender, RoutedEventArgs e)
-        {
-            // Obten el valor del DNI ingresado por el usuario
-            int dni;
-            if (int.TryParse(txtDNI.Text, out dni))
-            {
-                // Llama al método TraerClientePorDNI de ClasesBase para buscar el cliente en la base de datos
-                Cliente cliente = TrabajarClientes.TraerClientePorDNI(dni);
-
-                if (cliente != null)
-                {
-                    // Si se encuentra un cliente, carga automáticamente los campos de apellido, nombre y teléfono
-                    txtApellido.Text = cliente.Apellido;
-                    txtNombre.Text = cliente.Nombre;
-                    txtTelefono.Text = cliente.Telefono;
-                }
-                else
-                {
-                    // Si el cliente no se encontró, puedes borrar los campos o mostrar un mensaje de error.
-                    txtApellido.Text = string.Empty;
-                    txtNombre.Text = string.Empty;
-                    txtTelefono.Text = string.Empty;
-                }
-            }
-        }
-
-        private void close_Click(object sender, RoutedEventArgs e)
-        {
-            Content = new Clientes();
-        }
-
     }
 }
