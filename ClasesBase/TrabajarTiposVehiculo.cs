e8a321cd0b7230e235f7b0bdcdb278ce10c4669c
julimamani <65739081+julimamani@users.noreply.github.com>
2023-12-01 09:45:35 -0300

    Update TrabajarTiposVehiculo.cs

--- ClasesBase/TrabajarTiposVehiculo.cs
+++ ClasesBase/TrabajarTiposVehiculo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,63 +23,14 @@ namespace ClasesBase
                 return dt;
             }*/
             SqlConnection cn = new SqlConnection(ClasesBase.Properties.Settings.Default.playaConnectionString);
-            SqlCommand cmd = new SqlCommand("Select TVCodigo, Tarifa, Descripcion, Imagen from TipoVehiculo",cn);
+            SqlCommand cmd = new SqlCommand("Select TVCodigo, Tarifa, Descripcion , Imagen from TipoVehiculo",cn);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
             return dt;
         }
 
-        public static void insertarTipoVehiculo(TipoVehiculo tipoVehiculo)
-        {
-            try
-            {
-                using (SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.playaConnectionString))
-                {
-                    cnn.Open();
-
-                    // Verifica si el código ya existe en la base de datos
-                    using (SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM TipoVehiculo WHERE TVCodigo = @tvcodigo", cnn))
-                    {
-                        checkCmd.Parameters.AddWithValue("@tvcodigo", tipoVehiculo.TVCodigo);
-                        int existingRecords = (int)checkCmd.ExecuteScalar();
-
-                        if (existingRecords > 0)
-                        {
-                            // El código ya existe, lanza una excepción con un mensaje de error
-                            throw new Exception("El código de tipo de vehículo ya existe.");
-                        }
-                        else
-                        {
-                            // El código no existe, procede con la inserción
-                            using (SqlCommand cmd = new SqlCommand())
-                            {
-                                cmd.Connection = cnn;
-                                cmd.CommandType = CommandType.Text;
-                                cmd.CommandText = "INSERT INTO TipoVehiculo (Descripcion, Tarifa, TVCodigo, Imagen) VALUES (@descripcion, @tarifa, @tvcodigo, @imagen)";
-                                cmd.Parameters.AddWithValue("@descripcion", tipoVehiculo.Descripcion);
-                                cmd.Parameters.AddWithValue("@tarifa", tipoVehiculo.Tarifa);
-                                cmd.Parameters.AddWithValue("@tvcodigo", tipoVehiculo.TVCodigo);
-                                cmd.Parameters.AddWithValue("@imagen", tipoVehiculo.Imagen);
-                                cmd.ExecuteNonQuery();
-                                Console.WriteLine("Inserción exitosa");
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                string errorMessage = "Error al agregar el tipo de vehículo: " + ex.Message;
-                throw new Exception(errorMessage);
-            }
-            catch (Exception ex)
-            {
-                string errorMessage = "Se produjo un error. Por favor, contacta al administrador. " + ex.Message;
-                throw new Exception(errorMessage);
-            }
-        }
-
+        
 
 
       public static void EliminarTipoVehiculo(string codigoTipoVehiculo)
@@ -132,7 +83,6 @@ namespace ClasesBase
                       cmd.Parameters.AddWithValue("@tarifa", tipoVehiculo.Tarifa);
                       cmd.Parameters.AddWithValue("@tvcodigo", tipoVehiculo.TVCodigo);
                       cmd.Parameters.AddWithValue("@imagen", tipoVehiculo.Imagen);
-
                       cmd.ExecuteNonQuery();
                       Console.WriteLine("Modificación exitosa");
                   }
@@ -190,8 +140,57 @@ namespace ClasesBase
           return vehiculo;
 
       }
+      public static void insertarTipoVehiculo(TipoVehiculo tipoVehiculo)
+      {
+          try
+          {
+              using (SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.playaConnectionString))
+              {
+                  cnn.Open();
 
+                  // Verifica si el código ya existe en la base de datos
+                  using (SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM TipoVehiculo WHERE TVCodigo = @tvcodigo", cnn))
+                  {
+                      checkCmd.Parameters.AddWithValue("@tvcodigo", tipoVehiculo.TVCodigo);
+                      int existingRecords = (int)checkCmd.ExecuteScalar();
+
+                      if (existingRecords > 0)
+                      {
+                          // El código ya existe, lanza una excepción con un mensaje de error
+                          throw new Exception("El código de tipo de vehículo ya existe.");
+                      }
+                      else
+                      {
+                          // El código no existe, procede con la inserción
+                          using (SqlCommand cmd = new SqlCommand())
+                          {
+                              cmd.Connection = cnn;
+                              cmd.CommandType = CommandType.Text;
+                              cmd.CommandText = "INSERT INTO TipoVehiculo (Descripcion, Tarifa, TVCodigo , Imagen) VALUES (@descripcion, @tarifa, @tvcodigo, @imagen)";
+                              cmd.Parameters.AddWithValue("@descripcion", tipoVehiculo.Descripcion);
+                              cmd.Parameters.AddWithValue("@tarifa", tipoVehiculo.Tarifa);
+                              cmd.Parameters.AddWithValue("@tvcodigo", tipoVehiculo.TVCodigo);
+                              cmd.Parameters.AddWithValue("@imagen", tipoVehiculo.Imagen);
+                              cmd.ExecuteNonQuery();
+                              Console.WriteLine("Inserción exitosa");
+                          }
+                      }
+                  }
+              }
+          }
+          catch (SqlException ex)
+          {
+              string errorMessage = "Error al agregar el tipo de vehículo: " + ex.Message;
+              throw new Exception(errorMessage);
+          }
+          catch (Exception ex)
+          {
+              string errorMessage = "Se produjo un error. Por favor, contacta al administrador. " + ex.Message;
+              throw new Exception(errorMessage);
+          }
+      }
     }
+
 
     
 
